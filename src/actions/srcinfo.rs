f8a9ec1f55309392d17f83d51aabdc57b536dc49
John Peter Sa <156824256+johnpetersa19@users.noreply.github.com>
2026-06-02 16:02:14 -0300

    refactor(srcinfo): reuse shared regenerate_srcinfo helper and anyhow::Result

--- src/actions/srcinfo.rs
+++ src/actions/srcinfo.rs
@@ -1,30 +1,10 @@
 use std::path::Path;
-use std::process::Command;
-use std::fs::File;
-use std::io::Write;
 use super::get_target_dir;
+use super::regenerate_srcinfo;
 
-pub fn run(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+pub fn run(path: &Path) -> anyhow::Result<()> {
     let target_dir = get_target_dir(path)?;
-    println!("{} {:?}", gettextrs::gettext(">>> Regenerating .SRCINFO in"), target_dir);
-
-    let output = Command::new("makepkg")
-        .arg("--printsrcinfo")
-        .current_dir(&target_dir)
-        .output()?;
-
-    if !output.status.success() {
-        let err_msg = String::from_utf8_lossy(&output.stderr);
-        return Err(format!(
-            "{}: {}",
-            gettextrs::gettext("makepkg --printsrcinfo failed"),
-            err_msg
-        ).into());
-    }
-
-    let srcinfo_path = target_dir.join(".SRCINFO");
-    let mut file = File::create(srcinfo_path)?;
-    file.write_all(&output.stdout)?;
-
+    // Shared helper already logs and writes .SRCINFO for us
+    let _ = regenerate_srcinfo(&target_dir)?;
     Ok(())
 }
